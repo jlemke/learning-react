7af1872a743f06a88d7bcc594414d26401a871ad
jlemke <jlemke3@madisoncollege.edu>
2022-05-05 20:19:35 -0500

    Learned CSS in JS techniques

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 import Greeting from './Greeting';
 
 const App = () => {
+
+  const styles = {
+    container: {
+      padding: 30,
+      backgroundColor: new Date().getSeconds() % 2 === 0 ? "lightblue" : "pink"
+    },
+    heading: {
+      textTransform: 'uppercase'
+    }
+  };
+
   return (
-    <>
-      <h1>Hello React ! ! !</h1>
+    <div style={styles.container}>
+      <h1 style={styles.heading}>Hello React ! ! !</h1>
       <h2>Blorby</h2>
       <Greeting favoriteNumber={1} name={'Bordle'} favoriteColors={['red', 'red', 'red', 'green', 'blue']} />
       <Greeting favoriteNumber={4} name='Crusticus' />
       <Greeting favoriteNumber={22} name='Great Flimby' />
-    </>
+    </div>
   );
 };
 
