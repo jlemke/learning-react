8dbaea4b1d731330604aca07ef0c0d3641ca0990
jlemke <jlemke3@madisoncollege.edu>
2022-05-05 20:09:17 -0500

    destructured props
    changed props to an object

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,18 +1,22 @@
-const Greeting = (props) => {
-    console.log(props);
+const Greeting = ({
+    name,
+    favoriteNumber,
+    favoriteColors
+}) => {
+    //console.log(props);
     return (
       <>
-        <h2>Hello {props.name}...</h2>
-        <h3>{props.name}'s favorite number plus 10 is {props.favoriteNumber + 10}</h3>
+        <h2>Hello {name}...</h2>
+        <h3>{name}'s favorite number plus 10 is {favoriteNumber + 10}</h3>
         {
-            props.favoriteColors ?
-            <h3>{props.name}'s favorite colors:</h3> :
-            <h3>{props.name} doesn't have any favorite colors.</h3>
+            favoriteColors ?
+            <h3>{name}'s favorite colors:</h3> :
+            <h3>{name} doesn't have any favorite colors.</h3>
         }
         <ul>
             {
-                props.favoriteColors && 
-                [...new Set(props.favoriteColors)].map(x => (
+                favoriteColors && 
+                [...new Set(favoriteColors)].map(x => (
                     <li key={x}>{x}</li>
                 ))
             }
