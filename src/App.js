c24c47095be1cf9d1a477a1848187720c4b8a43f
jlemke <jlemke3@madisoncollege.edu>
2022-05-05 19:50:11 -0500

    Added another react prop
    Learned how to pass non-string values to components

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ const App = () => {
     <>
       <h1>Hello React ! ! !</h1>
       <h2>Blorby</h2>
-      <Greeting name='Bordle' />
-      <Greeting name='Crusticus' />
-      <Greeting name='Great Flimby' />
+      <Greeting favoriteNumber={1} name={'Bordle'} />
+      <Greeting favoriteNumber={4} name='Crusticus' />
+      <Greeting favoriteNumber={22} name='Great Flimby' />
     </>
   );
 };
--- src/Greeting.js
+++ src/Greeting.js
@@ -1,8 +1,10 @@
 const Greeting = (props) => {
-
     console.log(props);
     return (
-      <h2>Hello {props.name}...</h2>
+      <>
+        <h2>Hello {props.name}...</h2>
+        <h3>{props.name}'s favorite number plus 10 is {props.favoriteNumber + 10}</h3>
+      </>
     );
   };
 
