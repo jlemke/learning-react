c19c0c53fcbd5b09f1957f94f32a7ff6f5d2b827
jlemke <jlemke3@madisoncollege.edu>
2022-05-05 19:56:52 -0500

    Learned to use js in react apps
    used js logic to alter what html gets created

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
     <>
       <h1>Hello React ! ! !</h1>
       <h2>Blorby</h2>
-      <Greeting favoriteNumber={1} name={'Bordle'} />
+      <Greeting favoriteNumber={1} name={'Bordle'} favoriteColors={['red', 'green', 'blue']} />
       <Greeting favoriteNumber={4} name='Crusticus' />
       <Greeting favoriteNumber={22} name='Great Flimby' />
     </>
--- src/Greeting.js
+++ src/Greeting.js
@@ -4,6 +4,12 @@ const Greeting = (props) => {
       <>
         <h2>Hello {props.name}...</h2>
         <h3>{props.name}'s favorite number plus 10 is {props.favoriteNumber + 10}</h3>
+        {props.favoriteColors ?
+            <h3>{props.name}'s favorite colors:</h3> :
+            <h3>{props.name} doesn't have any favorite colors.</h3>}
+        <ul>
+            {props.favoriteColors && props.favoriteColors.map(x => <li>{x}</li>)}
+        </ul>
       </>
     );
   };
